290640ba5032f9a5193bff8da3214b8c40c0ac4f
ptenst <waaatt1984@gmail.com>
2023-05-18 16:04:38 +0800

    -

--- address_static_bind.cc
+++ address_static_bind.cc
@@ -18,7 +18,7 @@ void on_handle_something(void *hdl)
 {   
     Gamer *gamer = (Gamer *)hdl;
     gamer->Play();              // 静态绑定，也称作地址早绑定，在编译时就知道调用哪个函数
-                                // 此处调用Gamer的Play()，其内存分布只占一个int，因此把int强转后，game_的位置也是100
+                                // 此处调用Gamer的Play()，Gamer的内存分布只占一个int，因此把int强转后，game_的值就是100
 }
 
 int main()
