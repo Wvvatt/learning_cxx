aae53f5cc842239047aa083ca75f9f198fd81528
ptenst <waaatt1984@gmail.com>
2023-05-18 16:30:03 +0800

    -

--- raii_emplace_push.cc
+++ raii_emplace_push.cc
@@ -20,23 +20,24 @@ private:
 RaiiNoMove::RaiiNoMove():
     int_ptr_(new int(0))
 {
-    std::cout << "raii construct\n";
+    std::cout << " raii-no-move construct\n";
 }
 
 RaiiNoMove::~RaiiNoMove()
 {
+    std::cout << " raii-no-move destruct\n";
     delete int_ptr_;
 }
 
 RaiiNoMove::RaiiNoMove(const RaiiNoMove &rhs)
 {
-    std::cout << "raii copy\n";
+    std::cout << " raii-no-move copy\n";
     int_ptr_ = new int(*rhs.int_ptr_);
 }
 
 RaiiNoMove &RaiiNoMove::operator=(const RaiiNoMove &rhs)
 {
-    std::cout << "raii = copy\n";
+    std::cout << " raii-no-move = copy\n";
     int_ptr_ = new int(*rhs.int_ptr_);
 
     return *this;
@@ -58,23 +59,24 @@ private:
 RaiiWithMove::RaiiWithMove():
     int_ptr_(new int(0))
 {
-    std::cout << "raii construct\n";
+    std::cout << " raii-with-move construct\n";
 }
 
 RaiiWithMove::~RaiiWithMove()
 {
+    std::cout << " raii-with-move destruct\n";
     delete int_ptr_;
 }
 
 RaiiWithMove::RaiiWithMove(const RaiiWithMove &rhs)
 {
-    std::cout << "raii copy\n";
+    std::cout << " raii-with-move copy\n";
     int_ptr_ = new int(*rhs.int_ptr_);
 }
 
 RaiiWithMove &RaiiWithMove::operator=(const RaiiWithMove &rhs)
 {
-    std::cout << "raii = copy\n";
+    std::cout << " raii-with-move = copy\n";
     int_ptr_ = new int(*rhs.int_ptr_);
 
     return *this;
@@ -82,14 +84,14 @@ RaiiWithMove &RaiiWithMove::operator=(const RaiiWithMove &rhs)
 
 RaiiWithMove::RaiiWithMove(RaiiWithMove &&rhs)
 {
-    std::cout << "raii move\n";
+    std::cout << " raii-with-move move\n";
     int_ptr_ = rhs.int_ptr_;
     rhs.int_ptr_ = nullptr;
 }
 
 RaiiWithMove &RaiiWithMove::operator=(RaiiWithMove &&rhs)
 {
-    std::cout << "raii = move\n";
+    std::cout << " raii-with-move = move\n";
     int_ptr_ = rhs.int_ptr_;
     rhs.int_ptr_ = nullptr;
 
@@ -100,27 +102,43 @@ int main()
 {
     std::queue<RaiiNoMove> q1;
     std::cout << "queue------ \n";
-    std::cout << "raii no move : \n";
-    q1.emplace();
-    q1.emplace(RaiiNoMove());   // 退化为push，产生拷贝
-    q1.push(RaiiNoMove());
+    std::cout << "raii no move : \n\n";
+    q1.emplace();               // 元素是RaiiNoMove，只构造
+    std::cout << "\n";
+    q1.emplace(RaiiNoMove());   // 退化为push，先构造，再拷贝构造
+    std::cout << "\n";
+    q1.push(RaiiNoMove());      // 先构造，再拷贝构造
+    std::cout << "\n";
 
     std::queue<RaiiWithMove> q2;
-    std::cout << "raii with move : \n";
+    std::cout << "raii with move : \n\n";
     q2.emplace();
+    std::cout << "\n";
     q2.emplace(RaiiWithMove());
+    std::cout << "\n";
     q2.push(RaiiWithMove());
+    std::cout << "\n";
 
     std::unordered_map<int, RaiiNoMove> m1;
     std::cout << "map------ \n";
-    std::cout << "raii no move : \n";
-    m1.emplace(1, RaiiNoMove());
-    m1.emplace(std::make_pair(1, RaiiNoMove()));
-    m1.insert(std::make_pair(1, RaiiNoMove()));
+    std::cout << "raii no move : \n\n";
+    m1.emplace();                       // 元素是pair，构造pair，此时调用RaiiNoMove的默认构造
+    std::cout << "\n";
+    m1.emplace(1, RaiiNoMove());        // 构造RaiiNoMove -> 构造pair(RaiiNoMove拷贝构造)
+    std::cout << "\n";
+    m1.emplace(std::make_pair(2, RaiiNoMove()));        // 构造RaiiNoMove -> 构造pair(RaiiNoMove拷贝构造) -> 拷贝pair(RaiiNoMove拷贝构造)
+    std::cout << "\n";
+    m1.insert(std::make_pair(3, RaiiNoMove()));         // 与上述一致
+    std::cout << "\n";
     
     std::unordered_map<int, RaiiWithMove> m2;
-    std::cout << "raii with move : \n";
+    std::cout << "raii with move : \n\n";
+    m1.emplace();
+    std::cout << "\n";
     m2.emplace(1, RaiiWithMove());
-    m2.emplace(std::make_pair(1, RaiiWithMove()));
-    m2.insert(std::make_pair(1, RaiiWithMove()));
+    std::cout << "\n";
+    m2.emplace(std::make_pair(2, RaiiWithMove()));
+    std::cout << "\n";
+    m2.insert(std::make_pair(3, RaiiWithMove()));
+    std::cout << "\n";
 }
