b794f6538460caf2d05ad5db6a2446a7ecbb1c8c
wVVatt <445190686@qq.com>
2024-08-16 08:58:36 +0800

    -

--- packed_struct.cc
+++ packed_struct.cc
@@ -11,22 +11,36 @@ struct MyStruct
     short c;
 };
 
-struct MyPackedStruct
+struct MyGnuPackedStruct
 {
     void Print()
     {
-        std::cout << "MyPackedStruct: a= " << (void *)&a << ", b= " << &b << ", c= " << &c << std::endl;
+        std::cout << "MyGnuPackedStruct: a= " << (void *)&a << ", b= " << &b << ", c= " << &c << std::endl;
     }
     char a;
     int b;
     short c;
 } __attribute__((packed));
 
+#pragma pack(1)
+struct MyPragmaPackedStruct
+{
+    void Print()
+    {
+        std::cout << "MyPragmaPackedStruct: a= " << (void *)&a << ", b= " << &b << ", c= " << &c << std::endl;
+    }
+    char a;
+    int b;
+    short c;
+};
+#pragma pack()
 
 int main()
 {   
     MyStruct my_s;
-    MyPackedStruct my_ps;
+    MyGnuPackedStruct my_gnu_ps;
+    MyPragmaPackedStruct my_pragma_ps;
     my_s.Print();
-    my_ps.Print();
+    my_gnu_ps.Print();
+    my_pragma_ps.Print();
 }
